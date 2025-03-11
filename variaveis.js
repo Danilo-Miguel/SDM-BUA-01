cacb02034510c8f42147c9135c0b69e24b1489f7
Italo-Ribeiro-Prates <italoribeiroprates424@gmail.com>
2025-03-11 10:12:33 -0300

    feat: Trabalahando com variaveis

--- variaveis.js
+++ variaveis.js
@@ -3,6 +3,14 @@ let nome = "João";
 const pi = 3.14;
 let ativo = true;
 
+let num = Number("10");
+let num2 = parseFloat("10.5"); 
+let num3 = parseInt("10.7");
+
+console.log("é aqui "+num3);
+
+
+
 console.log(idade);
 console.log(nome);
 console.log(pi);

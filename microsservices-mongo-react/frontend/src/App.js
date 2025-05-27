d809e96ead4256c7e25fe3086995f56d22faaea4
Danilo-Miguel <prof.danilomiguel@ulife.com.br>
2025-05-27 10:02:44 -0300

    Feat: Create user form

--- microsservices-mongo-react/frontend/src/App.js
+++ microsservices-mongo-react/frontend/src/App.js
@@ -1,21 +1,25 @@
-import React from "react";
-import UserForm from "./components/UserForm";
-import UserList from "./components/UserList";
-import OrderList from "./components/OrderList";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Microsserviços: User e Order</h1>
-      <UserForm />
-      <hr />
-      <UserList />
-      <hr />
-      <OrderList />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
 export default App;
-
-
